72fabca4b922309ec2f85cce67138a0ece1da2c6
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-05 16:06:12 +0300

    Adding Backdrop module

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from "react";
+import { Fragment } from "react";
 
 import Header from "./components/Layout/Header";
 import Meal from "./components/Meals/Meal";
+import Cart from "./components/Cart/Cart";
 function App() {
   return (
-    <React.Fragment>
+    <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meal />
       </main>
-    </React.Fragment>
+    </Fragment>
   );
 }
 
