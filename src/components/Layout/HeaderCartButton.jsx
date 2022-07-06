b8ff810ba3cbb3268ae948f4b7f038bc853c72ab
halilibrahimcelik <halil.celik139@gmail.com>
2022-07-06 12:44:47 +0300

    configuring button animation via UseEffect.

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,36 @@
-import React,{useContext} from 'react'
+import React,{useContext,useEffect,useState} from 'react'
 
 import styles from "./headerCartButton.module.css";
 import {FaShoppingCart} from "react-icons/fa"
 import CartContext from '../../store/cart-context';
 const HeaderCartButton = (props) => {
+  const [isAnimated,setAnimated]=useState(false);
   const {onShowCart}=props;
   const cartCtx=useContext(CartContext);
   const numberOfCartItems=cartCtx.items.reduce((curNumber,item)=>{
     return curNumber+ item.amount
   },0);//!this fun reduce array into single number
   
+  //!Animation initiated everytime we add sth to cart
+  const {items}=cartCtx;
+  useEffect(() => {
+    if(items.length===0){
+      return
+    }
+    setAnimated(true);
+  
+    const timer=setTimeout(()=>{
+      setAnimated(false);
+    },300);
+    return ()=>{
+      //!memory leakage prevented
+      clearTimeout(timer)
+    }
+  }, [items])
+  
+  const btnClasses=`${styles.button} ${isAnimated&& styles.bump}`
   return (
-    <button className={styles.button} onClick={onShowCart} >
+    <button className={btnClasses} onClick={onShowCart} >
         <span className={styles.icon} > <FaShoppingCart/> </span>
         <span>Your Cart </span>
         <span className={styles.badge} > {numberOfCartItems} </span>
